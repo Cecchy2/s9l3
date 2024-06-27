482f867d6ca80354ec9bbf46bd60474c2f0370b7
Cecchy2 <dariocecchinato@gmail.com>
2024-06-27 14:06:26 +0200

    aggiunta selezione alle cards

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,14 +1,25 @@
+import { Component } from "react";
 import { Card } from "react-bootstrap";
 
-const SingleBook = (props) => (
-  <Card>
-    <Card.Img variant="top" src={props.img} />
-    <Card.Body>
-      <Card.Title>{props.title}</Card.Title>
-      <Card.Text>{props.price} &euro;</Card.Text>
-      <Card.Text>{props.category}</Card.Text>
-    </Card.Body>
-  </Card>
-);
+class SingleBook extends Component {
+  state = {
+    selected: false,
+  };
+  render() {
+    return (
+      <Card
+        onClick={(e) => this.setState({ selected: !this.state.selected })}
+        className={this.state.selected ? "selected-card" : ""}
+      >
+        <Card.Img variant="top" src={this.props.img} />
+        <Card.Body>
+          <Card.Title>{this.props.title}</Card.Title>
+          <Card.Text>{this.props.price} &euro;</Card.Text>
+          <Card.Text>{this.props.category}</Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
 
 export default SingleBook;
