d4e839e6423c633af6175d2bdbd341a84f1385b1
Cecchy2 <dariocecchinato@gmail.com>
2024-06-27 08:27:49 +0200

    correzione titolo

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,12 +1,10 @@
-import Alert from 'react-bootstrap/Alert';
+import Alert from "react-bootstrap/Alert";
 
 function Welcome() {
   return (
     <>
-    <h1>Benvenuti Al mio BookShoop</h1>
-      {[
-        'dark',
-      ].map((variant) => (
+      <h1>Benvenuti nel mio BookShoop</h1>
+      {["dark"].map((variant) => (
         <Alert key={variant} variant={variant}>
           Tutti i Libri al 50%
         </Alert>
